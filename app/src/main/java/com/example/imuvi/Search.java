4bcfa414035be0f34892af760eacc5de500d5cae
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2022-06-04 14:25:03 -0300

    Resolução de erros
    Foi tentado resolver um erro na lógica do json

--- app/src/main/java/com/example/imuvi/Search.java
+++ app/src/main/java/com/example/imuvi/Search.java
@@ -1,15 +1,138 @@
 package com.example.imuvi;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Search extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Search extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+    private EditText editSearch;
+    private TextView textTitulo;
+    private TextView textYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         getSupportActionBar().hide();
+        editSearch = findViewById(R.id.txt_search);
+        textTitulo = findViewById(R.id.txt_titulo);
+        textYear = findViewById(R.id.txt_ano);
+
+        if (getSupportLoaderManager().getLoader(0) != null) {
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
+    }
+
+    public void buscaLivros(View view) {
+        // Recupera a string de busca.
+        String queryString = textTitulo.getText().toString();
+        // esconde o teclado qdo o botão é clicado
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        // Verifica o status da conexão de rede
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+        /* Se a rede estiver disponivel e o campo de busca não estiver vazio
+         iniciar o Loader CarregaLivros */
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
+            textYear.setText(R.string.str_empty);
+            textTitulo.setText(R.string.loading);
+        }
+        // atualiza a textview para informar que não há conexão ou termo de busca
+        else {
+            if (queryString.length() == 0) {
+                textYear.setText(R.string.str_empty);
+                textTitulo.setText(R.string.no_search_term);
+            } else {
+                textYear.setText(" ");
+                textTitulo.setText(R.string.no_network);
+            }
+        }
+    }
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        String queryString = "";
+        if (args != null) {
+            queryString = args.getString("queryString");
+        }
+        return new CarregaFilmes(this, queryString);
+    }
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            // Converte a resposta em Json
+            JSONObject jsonObject = new JSONObject(data);
+            // Obtem o JSONArray dos itens de livros
+            JSONArray itemsArray = jsonObject.getJSONArray("object");
+            // inicializa o contador
+            int i = 0;
+            String titulo = null;
+            String ano = null;
+            // Procura pro resultados nos itens do array
+            while (i < itemsArray.length() &&
+                    (ano == null && titulo == null)) {
+                // Obtem a informação
+                JSONObject book = itemsArray.getJSONObject(i);
+
+                //  Obter autor e titulo para o item,
+                // erro se o campo estiver vazio
+                try {
+                    titulo = book.getString("Title");
+                    ano = book.getString("Year");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                // move para a proxima linha
+                i++;
+            }
+            //mostra o resultado qdo possivel.
+            if (titulo != null && ano != null) {
+                textTitulo.setText(titulo);
+                textYear.setText(ano);
+                //nmLivro.setText(R.string.str_empty);
+            } else {
+                // If none are found, update the UI to show failed results.
+                textTitulo.setText(R.string.no_results);
+                textYear.setText(R.string.str_empty);
+            }
+        } catch (Exception e) {
+            // Se não receber um JSOn valido, informa ao usuário
+            textTitulo.setText(R.string.no_results);
+            textYear.setText(R.string.str_empty);
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+        // obrigatório implementar, nenhuma ação executada
     }
 }
