6c579d6182098b998f48fc1783cc5bb156e97b76
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2022-06-14 17:51:04 -0300

    Add classes

--- app/src/main/java/com/example/imuvi/DBHelper.java
+++ app/src/main/java/com/example/imuvi/DBHelper.java
@@ -1,6 +1,8 @@
 package com.example.imuvi;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -122,5 +124,183 @@ public class DBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
+    public String addMovie(Movie movie){
+
+        //INSERINDO DADOS NA TABELA MOVIE
+
+        long resultado;
+
+        SQLiteDatabase db=this.getWritableDatabase();
+
+        ContentValues values= new ContentValues();
+        values.put(COLUMN_ID_IMDB, movie.getImdbId());
+        values.put(COLUMN_TITLE, movie.getTitle());
+        values.put(COLUMN_YEAR, movie.getYear());
+        values.put(COLUMN_RATED, movie.getRated());
+
+        values.put(COLUMN_RELEASED, movie.getReleased());
+        values.put(COLUMN_RUNTIME, movie.getRuntime());
+        values.put(COLUMN_GENRE, movie.getGenre());
+        values.put(COLUMN_DIRECTOR, movie.getDirector());
+        values.put(COLUMN_WRITER, movie.getWriter());
+        values.put(COLUMN_ACTORS, movie.getActors());
+
+        values.put(COLUMN_PLOT, movie.getPlot());
+        values.put(COLUMN_LANGUAGE, movie.getLanguage());
+        values.put(COLUMN_COUNTRY, movie.getCountry());
+        values.put(COLUMN_AWARDS, movie.getAwards());
+        values.put(COLUMN_POSTER, movie.getPoster());
+        values.put(COLUMN_METASCORE, movie.getMetascore());
+
+        values.put(COLUMN_IMDBRATING, movie.getImdbrating());
+        values.put(COLUMN_IMDBVOTES, movie.getImdbvotes());
+        values.put(COLUMN_TYPE, movie.getType());
+
+        values.put(COLUMN_DVD, movie.getDvd());
+        values.put(COLUMN_BOXOFFICE, movie.getBoxoffice());
+        values.put(COLUMN_PRODUCTION, movie.getProduction());
+        values.put(COLUMN_WEBSITE, movie.getWebsite());
+
+        resultado = db.insert(MOVIE_TABLE_NAME, null, values);
+
+        //FECHANDO O BANCO
+        db.close();
+
+        //RETORNANDO MENSAGENS
+        if (resultado ==-1) {
+            return "Erro ao inserir os dados de Filmes";
+        }else{
+            return "Dados de Filmes Inseridos com sucesso";
+        }
+
+
+    }
+
+    public String addRatings(Ratings ratings){
+        SQLiteDatabase db=this.getWritableDatabase();
+
+
+
+        //INSERINDO DADOS NA TABELA RATINGS
+        long resultadoR;
+        ContentValues valuesR= new ContentValues();
+
+        valuesR.put(COLUMN_ID_IMDBR, ratings.getIdimdbr());
+        valuesR.put(COLUMN_SOURCE, ratings.getSource());
+        valuesR.put(COLUMN_VALUE, ratings.getValue());
+
+        resultadoR = db.insert(RATINGS_TABLE_NAME, null, valuesR);
+
+        //FECHANDO O BANCO
+        db.close();
+
+        //RETORNANDO MENSAGENS
+        if (resultadoR ==-1) {
+            return "Erro ao inserir os dados de Avaliações";
+        }else{
+            return "Dados de Avaliações Inseridos com sucesso";
+        }
+
+
+    }
+
+    Movie buscaMovie(String idimdb){
+        SQLiteDatabase db=this.getReadableDatabase();
+
+        Cursor cursor=db.query(MOVIE_TABLE_NAME,
+                new String[]{COLUMN_ID_IMDB, COLUMN_TITLE, COLUMN_YEAR, COLUMN_RATED, COLUMN_RELEASED, COLUMN_RUNTIME,
+                        COLUMN_GENRE, COLUMN_DIRECTOR, COLUMN_WRITER, COLUMN_ACTORS, COLUMN_PLOT, COLUMN_LANGUAGE, COLUMN_COUNTRY,
+                        COLUMN_AWARDS, COLUMN_POSTER, COLUMN_METASCORE, COLUMN_IMDBRATING, COLUMN_IMDBVOTES, COLUMN_TYPE, COLUMN_DVD,
+                        COLUMN_BOXOFFICE, COLUMN_PRODUCTION, COLUMN_WEBSITE},
+                COLUMN_ID_IMDB+"=?",new String[]{String.valueOf(idimdb)},null, null, null,null);
+        if(cursor!=null && cursor.getCount()>0){
+            cursor.moveToFirst();
+        }
+
+        else if(cursor.getCount() == 0){
+            Movie filmeEspecifico = new Movie(
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists",
+                    "notExists"
+            );
+            return filmeEspecifico;
+        }
+
+
+        Movie filmeEspecifico= new Movie(
+                cursor.getString(0),
+                cursor.getString(1),
+                cursor.getString(2),
+                cursor.getString(3),
+                cursor.getString(4),
+                cursor.getString(5),
+                cursor.getString(6),
+                cursor.getString(7),
+                cursor.getString(8),
+                cursor.getString(9),
+                cursor.getString(10),
+                cursor.getString(11),
+                cursor.getString(12),
+                cursor.getString(13),
+                cursor.getString(14),
+                cursor.getString(15),
+                cursor.getString(16),
+                cursor.getString(17),
+                cursor.getString(18),
+                cursor.getString(19),
+                cursor.getString(20),
+                cursor.getString(21),
+                cursor.getString(22)
+        );
+        return filmeEspecifico;
+
+    }
+
+    Ratings buscaRatings(String idimdbr){
+        SQLiteDatabase db=this.getReadableDatabase();
+
+        Cursor cursor=db.query(RATINGS_TABLE_NAME,
+                new String[]{COLUMN_ID_IMDBR, COLUMN_SOURCE, COLUMN_VALUE},
+                COLUMN_ID_IMDBR+"=?",new String[]{String.valueOf(idimdbr)},null, null, null,null);
+        if(cursor!=null && cursor.getCount()>0){
+            cursor.moveToFirst();
+        }
+
+        else if(cursor.getCount() == 0){
+            Ratings ratingEspecifico = new Ratings(
+                    "notExists",
+                    "notExists",
+                    "notExists");
+            return ratingEspecifico;
+        }
+
+
+        Ratings ratingEspecifico= new Ratings(
+                cursor.getString(0),
+                cursor.getString(1),
+                cursor.getString(2));
+        return ratingEspecifico;
+
+    }
 
 }
